5fcccf73117ecc07951c92a7c69cf8629310cf35
DandyJoshua14 <dandyjoshua14@gmail.com>
2023-06-13 10:29:40 +0100

    Modified Live Preview Settings

--- src/lib/authorise.ts
+++ src/lib/authorise.ts
@@ -2,11 +2,10 @@ import { redirect, fail } from '@sveltejs/kit';
 
 export async function ensureLogin(page) {
 	try {
-		if (page != null) {
+		if (page) {
 			console.log('User Is Logged In');
 		} else {
-			console.log('User is Logged Out');
-			throw redirect(302, '/Login');
+			console.log("User Is'nt Logged In");
 		}
 	} catch {
 		return fail(500);
