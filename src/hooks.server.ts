84f535d665c2353ed5acbf2d8163bd86915e8c21
DandyJoshua14 <dandyjoshua14@gmail.com>
2023-11-26 18:57:58 +0100

    MOD

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -9,27 +9,47 @@ import { createContext } from '$lib/trpc/context';
 import { router } from '$lib/trpc/router';
 import { createTRPCHandle } from 'trpc-sveltekit';
 import { sequence } from '@sveltejs/kit/hooks';
+import { v4 as uuid } from 'uuid';
 
+let uuidv4 = uuid();
+let name = `User-${uuidv4}`;
+let name1 = `User-${uuidv4}`;
+let name2 = `User-${uuidv4}`;
+let name3 = `User-${uuidv4}`;
+let name4 = `User-${uuidv4}`;
+let name5 = `User-${uuidv4}`;
+let name6 = `User-${uuidv4}`;
+console.log(name, name1, name2, name3, name4, name5, name6);
 dotenv.config();
 
+// async function connectToDB() {
+// 	if (dev) {
+// 		await mongoose
+// 			.connect(`${process.env.MONGO_URL}`)
+// 			.then(() => console.log('Connected To Local Database.'))
+// 			.catch((e) => {
+// 				console.log(`Connection to Local Database Failed: ${e}`);
+// 			});
+// 	} else {
+// 		await mongoose
+// 			.connect(`${process.env.DOTENV_KEY}`)
+// 			.then(() => console.log('Connected To Online Database.'))
+// 			.catch((e) => {
+// 				if (e instanceof Query) {
+// 					console.log(`Connection to Online Database Failed: Network Error`);
+// 				}
+// 			});
+// 	}
+// }
 async function connectToDB() {
-	if (dev) {
-		await mongoose
-			.connect(`${process.env.MONGO_URL}`)
-			.then(() => console.log('Connected To Local Database.'))
-			.catch((e) => {
-				console.log(`Connection to Local Database Failed: ${e}`);
-			});
-	} else {
-		await mongoose
-			.connect(`${process.env.DOTENV_KEY}`)
-			.then(() => console.log('Connected To Online Database.'))
-			.catch((e) => {
-				if (e instanceof Query) {
-					console.log(`Connection to Online Database Failed: Network Error`);
-				}
-			});
-	}
+	await mongoose
+		.connect(`${process.env.DOTENV_KEY}`)
+		.then(() => console.log('Connected To Online Database.'))
+		.catch((e) => {
+			if (e instanceof Query) {
+				console.log(`Connection to Online Database Failed: Network Error`);
+			}
+		});
 }
 connectToDB();
 
@@ -39,6 +59,8 @@ const userSchema = new mongoose.Schema(
 		password: String,
 		email: String,
 		business_name: String,
+		inventory: Array,
+		invoices: Array,
 		_id: {
 			type: String
 		}
